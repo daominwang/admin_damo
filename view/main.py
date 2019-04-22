1ff2f9084aebce1d725a1c04dd5d4ea1cc581d14
wangdaomin@mshare.cn <wangdaomin@mshare.cn>
2019-04-23 00:20:13 +0800

    完成用户管理

--- view/main.py
+++ view/main.py
@@ -6,6 +6,7 @@
 # @File    : main.py 
 # @Software: PyCharm
 from . import view
+from app import db
 from model.admin import Admin
 from flask_login import login_required
 from flask import request, render_template, abort, json, jsonify
@@ -27,18 +28,63 @@ def index():
 def user_manage(user_type):
     if user_type == 'admin':
         if request.method == 'POST':
-            page = request.values.get('page', 1)
-            limit = request.values.get('limit', 10)
-            _admin_list = Admin.query.order_by(Admin.last_login_time.desc()).paginate(int(page), int(limit)).items
-            admin_list = []
-            for item in _admin_list:
-                admin_list.append({
-                    'id': item.id,
-                    'username': item.username,
-                    'is_alive': item.is_alive,
-                    'last_login_time': item.last_login_time.strftime('%Y-%m-%d %H:%M:%S') if item.last_login_time else None
-                })
-            return json.dumps({'code': 200, 'data': admin_list})
+            o_type = request.values.get('type')
+            if o_type == 'list':
+                key_word = request.values.get('key_word')
+                page = request.values.get('page', 1)
+                limit = request.values.get('limit', 10)
+                if key_word:
+                    _admin_list = Admin.query.filter(Admin.username.like(f'%{key_word}%'))
+                else:
+                    _admin_list = Admin.query
+                _admin_list = _admin_list.order_by(Admin.last_login_time.desc()).paginate(int(page), int(limit)).items
+                admin_list = []
+                for item in _admin_list:
+                    admin_list.append({
+                        'id': item.id,
+                        'username': item.username,
+                        'is_alive': item.is_alive,
+                        'last_login_time': item.last_login_time.strftime('%Y-%m-%d %H:%M:%S') if item.last_login_time else None
+                    })
+                return json.dumps({'code': 200, 'data': admin_list, 'count': Admin.query.count()})
+            elif o_type == 'modify':
+                _id = request.values.get('id')
+                user_name = request.values.get('username')
+                if not _id or not user_name:
+                    return json.dumps({'code': 500, 'msg': '参数错误'})
+                admin = Admin.query.get(_id)
+                if not admin:
+                    return json.dumps({'code': 500, 'msg': '网络异常，请稍候重试'})
+                admin.username = user_name
+                db.session.commit()
+                return json.dumps({'code': 200, 'msg': '修改成功'})
+            elif o_type == 'destroy':
+                _id = request.values.get('id')
+                if not _id:
+                    return json.dumps({'code': 500, 'msg': '参数错误'})
+                admin = Admin.query.get(_id)
+                admin.is_alive = False
+                db.session.commit()
+                return json.dumps({'code': 200, 'msg': '删除账号成功'})
+            elif o_type == 'online':
+                _id = request.values.get('id')
+                if not _id:
+                    return json.dumps({'code': 500, 'msg': '参数错误'})
+                admin = Admin.query.get(_id)
+                admin.is_alive = True
+                db.session.commit()
+                return json.dumps({'code': 200, 'msg': '恢复账号成功'})
+            elif o_type == 'add':
+                user_name = request.values.get('username')
+                password = request.values.get('password')
+                if not user_name or not password:
+                    return json.dumps({'code': 500, 'msg': '参数错误'})
+                admin = Admin()
+                admin.username = user_name
+                admin.set_password(password)
+                db.session.add(admin)
+                db.session.commit()
+                return json.dumps({'code': 200, 'msg': '添加成功'})
     elif user_type == 'user':
         pass
     else:
